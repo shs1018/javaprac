72a65428596b3bd90eeb1f286f761a1604fc7bab
shs1018 <1018gustj@naver.com>
2022-03-27 22:42:55 +0900

    java start

---
+++ src/Prac/Prac.java
@@ -0,0 +1,8 @@
+package Prac;
+import java.util.Scanner;	// Scanner 클래스 호출
+public class Prac {
+    public static void main(String[] args){
+        System.out.printf("age:%d%nyear:%d%n", 12, 2017);
+    }
+
+}
